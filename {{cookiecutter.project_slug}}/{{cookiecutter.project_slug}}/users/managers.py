852d177249cac1066dcbaa5d7b60532347ed9aea
Kevin Mills <millsks@gmail.com>
2023-06-23 16:55:28 -0500

    Update managers.py to use typing.Union
    Annotations are not supported until Python 3.10.  Updating managers.py to use typing.Union instead of "|" allows us to use cookiecutter-django with Python 3.9.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/managers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/managers.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import UserManager as DjangoUserManager
+from typing import Union
 
 
 class UserManager(DjangoUserManager):
     """Custom manager for the User model."""
 
-    def _create_user(self, email: str, password: str | None, **extra_fields):
+    def _create_user(self, email: str, password: Union[str, None], **extra_fields):
         """
         Create and save a user with the given email and password.
         """
@@ -17,12 +18,12 @@ class UserManager(DjangoUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email: str, password: str | None = None, **extra_fields):
+    def create_user(self, email: str, password: Union[str, None] = None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email: str, password: str | None = None, **extra_fields):
+    def create_superuser(self, email: str, password: Union[str, None] = None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
